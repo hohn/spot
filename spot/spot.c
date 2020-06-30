71b4cc7210798d8f4a7fa54b389c2ae3fda9408c
Logan McLintock <loganpkg@gmail.com>
2020-06-30 12:28:15 +0000

    spot: add get_filesize and init_buffer functions

--- spot/spot.c
+++ spot/spot.c
@@ -19,8 +19,13 @@
  * Dedicated to my son who was only a 4mm `spot' in his first ultrasound.
  */
 
+
+#include <sys/stat.h>
+#include <stdint.h>
 #include <string.h>
 
+
+#define GAP 2
 
 /* #ifdef _WIN32 */
 
@@ -74,6 +79,37 @@ int backspace_char(struct buffer *b, size_t mult)
     if (mult > (size_t) (b->g - b->a)) return 1;
     b->g -= mult;
     return 0;
+}
+
+int get_filesize(char *fn, size_t *fs)
+{
+    struct _stat64 st;
+    if (_stat64(fn, &st)) return 1;
+    if (st.st_size > SIZE_MAX || st.st_size < 0) return 1;
+    *fs = (size_t) st.st_size;
+    return 0;
+}
+
+struct buffer *init_buffer(size_t req)
+{
+    struct buffer *b;
+    if ((b = malloc(sizeof(struct buffer))) == NULL) return NULL;
+    if (req > SIZE_MAX - GAP) {
+        free(b);
+        return NULL;
+    }
+    if ((b->a = malloc(req + GAP)) == NULL) {
+        free(b);
+        return NULL;
+    }
+    b->fn = NULL;
+    b->g = b->a;
+    *(b->e = b->a + req + GAP - 1) = '~';
+    b->c = b->e;
+    b->d = b->a;
+    b->m = 0;
+    b->m_set = 0;
+    return b;
 }
 
 int main (void)
